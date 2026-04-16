1d1730fdc2b3edb8657e53736e37da738c66c06c
Usuario <Usuario@B6-501-26>
2026-04-16 17:19:33 -0500

    Agregar archivos de proyecto.

---
+++ MathService/MathService.asmx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace MathService
+{
+    /// <summary>
+    /// Descripción breve de MathService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class MathService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public int Add(int a, int b)
+        {
+            return (a + b);
+        }
+        [WebMethod]
+        public System.Single Subtract(System.Single A, System.Single B)
+        {
+            return (A - B);
+        }
+        [WebMethod]
+        public System.Single Multiply(System.Single A, System.Single B)
+        {
+            return A * B;
+        }
+        [WebMethod]
+        public System.Single Divide(System.Single A, System.Single B)
+        {
+            if (B == 0) return -1;
+            return Convert.ToSingle(A / B);
+        }
+    }
+}
